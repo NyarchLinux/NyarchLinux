10861f0dcb6c742018eaf58877b0cb7df7779ce7
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2023-05-10 18:53:40 +0200

    Set nautilus as default file browser

--- Gnome/usr/share/ezarcher/Scripts/autostart.sh
+++ Gnome/usr/share/ezarcher/Scripts/autostart.sh
@@ -2,6 +2,7 @@
 
 LIVEUSER="live"
 chmod -R 777 ~/.local/share/gnome-shell/extensions
+xdg-mime default org.gnome.Nautilus.desktop inode/directory
 if [ "$USER" = "$LIVEUSER" ]; then
     sh -c "pkexec calamares --style adwaita"
 else
