3b0e677864296fa1172f2cfb773cee6d6a0b8a97
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2023-05-10 18:31:51 +0200

    Change Calamares customization
    - Make Calamares use Adwaita theme
    - Change slides for 23.05 release

--- Gnome/usr/share/ezarcher/Scripts/autostart.sh
+++ Gnome/usr/share/ezarcher/Scripts/autostart.sh
@@ -3,7 +3,7 @@
 LIVEUSER="live"
 chmod -R 777 ~/.local/share/gnome-shell/extensions
 if [ "$USER" = "$LIVEUSER" ]; then
-    sh -c "pkexec calamares"
+    sh -c "pkexec calamares --style adwaita"
 else
     flatpak run moe.nyarchlinux.tour
     rm -rf ~/.config/autostart/start.desktop
