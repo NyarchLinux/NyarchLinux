0342b7a05e35444b275b4670afce38df6150f5d3
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2023-10-09 21:59:38 +0200

    Remove adwaita style from calamares

--- Gnome/usr/share/ezarcher/Scripts/autostart.sh
+++ Gnome/usr/share/ezarcher/Scripts/autostart.sh
@@ -4,7 +4,7 @@ LIVEUSER="live"
 chmod -R 777 ~/.local/share/gnome-shell/extensions
 xdg-mime default org.gnome.Nautilus.desktop inode/directory
 if [ "$USER" = "$LIVEUSER" ]; then
-    sh -c "pkexec calamares --style adwaita"
+    sh -c "pkexec calamares"
 else
     flatpak run moe.nyarchlinux.tour
     rm -rf ~/.config/autostart/start.desktop
