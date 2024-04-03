3901a805d50ab63000b7459a42c97e23fbe6421b
root <francescocaracciolo78@gmail.com>
2024-04-03 19:36:46 +0200

    Change extensions settings

--- Gnome/usr/share/ezarcher/Scripts/autostart.sh
+++ Gnome/usr/share/ezarcher/Scripts/autostart.sh
@@ -4,7 +4,9 @@ LIVEUSER="live"
 chmod -R 777 ~/.local/share/gnome-shell/extensions
 xdg-mime default org.gnome.Nautilus.desktop inode/directory
 if [ "$USER" = "$LIVEUSER" ]; then
-    sh -c "pkexec calamares"
+   sleep 5 
+   sh -c "pkexec calamares"
+   echo "Calamares creates issues when auto launched"
 else
     flatpak run moe.nyarchlinux.tour
     rm -rf ~/.config/autostart/start.desktop
