e8218c899c8e50bdf2290112472687e2e6864d1d
FrancescoCaracciolo <francescocaracciolo78@gmail.com>
2024-04-08 20:20:24 +0200

    Fix icon theme not applying

--- Gnome/usr/share/ezarcher/Scripts/autostart.sh
+++ Gnome/usr/share/ezarcher/Scripts/autostart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 LIVEUSER="live"
+chmor -R 777 ~/.config/nyarch
 chmod -R 777 ~/.local/share/gnome-shell/extensions
 xdg-mime default org.gnome.Nautilus.desktop inode/directory
 if [ "$USER" = "$LIVEUSER" ]; then
