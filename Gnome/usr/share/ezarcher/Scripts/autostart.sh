e260152151e0fec2faabab5ed67af7909996335a
Francesco Caracciolo <francescocaracciolo78@gmail.com>
2024-07-17 14:44:16 +0200

    Fix calamares starting two times

--- Gnome/usr/share/ezarcher/Scripts/autostart.sh
+++ Gnome/usr/share/ezarcher/Scripts/autostart.sh
@@ -6,8 +6,7 @@ chmod -R 777 ~/.local/share/gnome-shell/extensions
 xdg-mime default org.gnome.Nautilus.desktop inode/directory
 if [ "$USER" = "$LIVEUSER" ]; then
    sleep 5 
-   sh -c "pkexec calamares"
-   echo "Calamares creates issues when auto launched"
+   #sh -c "pkexec calamares"
 else
     flatpak run moe.nyarchlinux.tour
     rm -rf ~/.config/autostart/start.desktop
