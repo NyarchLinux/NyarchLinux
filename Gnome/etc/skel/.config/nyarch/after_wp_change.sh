0676c1fccb4c4d0ae64a5c818a6e9ed259852fda
Francesco Caracciolo <francescocaracciolo78@gmail.com>
2024-07-18 10:00:17 +0200

    Fix first wallpaper change causes gnome shell crash

--- Gnome/etc/skel/.config/nyarch/after_wp_change.sh
+++ Gnome/etc/skel/.config/nyarch/after_wp_change.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # Get the current color from pywal
 #PYCOLOR=$(jq -r '.colors.color2' < ~/.cache/wal/colors.json | sed 's/#//')
-sleep 3
-file_name=~/.config/gtk-4.0/gtk.css  # Replace with the actual file path
-COLOR=$(grep 'accent_bg_color' $file_name | cut -d ' ' -f 3 | tr -d ';')
-echo $COLOR;
-# Generate and install tela icons
-~/.config/nyarch/Tela-circle-icon-theme-custom-color/install.sh -a "$COLOR" -n Tela-circle-MaterialYou
-gsettings set org.gnome.desktop.interface icon-theme "Adwaita"
-gsettings set org.gnome.desktop.interface icon-theme "Tela-circle-MaterialYou"
+execute_change () {
+	sleep 1
+	file_name=~/.config/gtk-4.0/gtk.css  # Replace with the actual file path
+	COLOR=$(grep 'accent_bg_color' $file_name | cut -d ' ' -f 3 | tr -d ';')
+	echo $COLOR;
+	# Generate and install tela icons
+	chmod +x ~/.config/nyarch/Tela-circle-icon-theme-custom-color/install.sh 
+	~/.config/nyarch/Tela-circle-icon-theme-custom-color/install.sh -a "$COLOR" -n Tela-circle-MaterialYou
+	gsettings set org.gnome.desktop.interface icon-theme "Adwaita"
+	gsettings set org.gnome.desktop.interface icon-theme "Tela-circle-MaterialYou"
+}
+
+execute_change &
