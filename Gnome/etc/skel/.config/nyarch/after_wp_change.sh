ac2813b5dfdfd2fd1f1b122bd22baa9e15fc86e3
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2024-10-28 12:22:32 +0100

    Faster and better icons switch

--- Gnome/etc/skel/.config/nyarch/after_wp_change.sh
+++ Gnome/etc/skel/.config/nyarch/after_wp_change.sh
@@ -2,15 +2,16 @@
 # Get the current color from pywal
 #PYCOLOR=$(jq -r '.colors.color2' < ~/.cache/wal/colors.json | sed 's/#//')
 execute_change () {
-	sleep 1
 	file_name=~/.config/gtk-4.0/gtk.css  # Replace with the actual file path
 	COLOR=$(grep 'accent_bg_color' $file_name | cut -d ' ' -f 3 | tr -d ';')
 	echo $COLOR;
 	# Generate and install tela icons
 	chmod +x ~/.config/nyarch/Tela-circle-icon-theme-custom-color/install.sh 
-	~/.config/nyarch/Tela-circle-icon-theme-custom-color/install.sh -a "$COLOR" -n Tela-circle-MaterialYou
-	gsettings set org.gnome.desktop.interface icon-theme "Adwaita"
-	gsettings set org.gnome.desktop.interface icon-theme "Tela-circle-MaterialYou"
+	~/.config/nyarch/Tela-circle-icon-theme-custom-color/install.sh -a "$COLOR" -n Tela-circle-MaterialYou-$COLOR
+	#gsettings set org.gnome.desktop.interface icon-theme "Adwaita"
+	gsettings set org.gnome.desktop.interface icon-theme "Tela-circle-MaterialYou-$COLOR"
+  	#gtk-update-icon-cache
+  	find ~/.local/share/icons -type d -name "Tela-circle-MaterialYou-*" ! -name "Tela-circle-MaterialYou-$COLOR" -exec rm -rf "{}" +
 }
 
 execute_change &
