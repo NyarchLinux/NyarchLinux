99c6965ebc81bf6bced94c7745e8d1fddaab2097
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2024-10-28 15:14:43 +0100

    Fix ucin generation with light theme

--- Gnome/etc/skel/.config/nyarch/after_wp_change.sh
+++ Gnome/etc/skel/.config/nyarch/after_wp_change.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 # Get the current color from pywal
 #PYCOLOR=$(jq -r '.colors.color2' < ~/.cache/wal/colors.json | sed 's/#//')
+# Check if is dark
+ISDARK=$(gsettings get org.gnome.desktop.interface color-scheme)
 execute_change () {
 	file_name=~/.config/gtk-4.0/gtk.css  # Replace with the actual file path
-	COLOR=$(grep 'accent_bg_color' $file_name | cut -d ' ' -f 3 | tr -d ';')
-	echo $COLOR;
+	echo $ISDARK
+  if [ "$ISDARK" = "'default'" ]; then
+    COLOR=$(grep 'accent_color' "$file_name" | cut -d ' ' -f 3 | tr -d ';')
+  else
+    COLOR=$(grep 'accent_bg_color' "$file_name" | cut -d ' ' -f 3 | tr -d ';')
+  fi
+  echo $COLOR;
 	# Generate and install tela icons
 	chmod +x ~/.config/nyarch/Tela-circle-icon-theme-custom-color/install.sh 
 	~/.config/nyarch/Tela-circle-icon-theme-custom-color/install.sh -a "$COLOR" -n Tela-circle-MaterialYou-$COLOR
 	#gsettings set org.gnome.desktop.interface icon-theme "Adwaita"
 	gsettings set org.gnome.desktop.interface icon-theme "Tela-circle-MaterialYou-$COLOR"
-  	#gtk-update-icon-cache
-  	sleep 10
-  	find ~/.local/share/icons -type d -name "Tela-circle-MaterialYou-*" ! -name "Tela-circle-MaterialYou-$COLOR" -exec rm -rf "{}" +
+  #gtk-update-icon-cache
+  sleep 10
+  find ~/.local/share/icons -type d -name "Tela-circle-MaterialYou-*" ! -name "Tela-circle-MaterialYou-$COLOR" -exec rm -rf "{}" +
 }
 
 execute_change &
