4567631abfca3f68a3903f74334b7a48e834011f
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2024-11-08 22:24:36 +0100

    Disable suspension for live user

--- Gnome/usr/share/ezarcher/Scripts/autostart.sh
+++ Gnome/usr/share/ezarcher/Scripts/autostart.sh
@@ -5,7 +5,8 @@ chmor -R 777 ~/.config/nyarch
 chmod -R 777 ~/.local/share/gnome-shell/extensions
 xdg-mime default org.gnome.Nautilus.desktop inode/directory
 if [ "$USER" = "$LIVEUSER" ]; then
-   sleep 5 
+   # Disable suspension
+   gsettings set org.gnome.desktop.session idle-delay "uint32 0"
    #sh -c "pkexec calamares"
 else
     flatpak run moe.nyarchlinux.tour
