46f780a963027a10f2e2cfc8fd811d0bc9e50e0f
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2025-12-18 12:04:34 +0100

    Update version

--- Gnome/etc/skel/.local/share/gnome-shell/extensions/material-you-colors@francescocaracciolo.github.io/adwaita-material-you/extension_integration.py
+++ Gnome/etc/skel/.local/share/gnome-shell/extensions/material-you-colors@francescocaracciolo.github.io/adwaita-material-you/extension_integration.py
@@ -15,7 +15,7 @@ ARCMENU_SCHEMA = "org.gnome.shell.extensions.arcmenu"
 EXTENSION_UUID = "material-you-colors@francescocaracciolo.github.io"
 EXTENSIONDIR = "~/.local/share/gnome-shell/extensions/" + EXTENSION_UUID
 EXTENSION_SCHEMA = "org.gnome.shell.extensions.material-you-colors"
-VERSION = 48
+VERSION = 49
 
 COLOR_TO_ACCENT = {
   0xffbc9769: "orange",
