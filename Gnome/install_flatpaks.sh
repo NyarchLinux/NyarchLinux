b8461c2101c3bfcf25066e81b4d186bdebd452cd
spebelgenenst <sebastian@die-sperlinge.de>
2025-12-26 20:46:08 +0100

    modified:   Gnome/install_flatpaks.sh

--- Gnome/install_flatpaks.sh
+++ Gnome/install_flatpaks.sh
@@ -3,30 +3,18 @@
 # This scripts downloads default Nyarch flatpak on host machine. 
 # Note: by default, steps.sh will add to the iso every flatpak in the host.
 
-flatpak install flathub info.febvre.Komikku
-flatpak install flathub com.github.tchx84.Flatseal
-#flatpak install flathub de.haeckerfelix.Shortwave
-flatpak install org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark
-flatpak install flathub com.mattjakeman.ExtensionManager
-flatpak install flathub it.mijorus.gearlever
+flatpak install flathub info.febvre.Komikku com.github.tchx84.Flatseal de.haeckerfelix.Shortwave org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark com.mattjakeman.ExtensionManager it.mijorus.gearlever
 
+mkdir flatpaks
 cd flatpaks
-rm -rf nyarchtour.flatpak
-rm -rf catgirldownloader.flatpak
-rm -rf nyarchcustomize.flatpak
-rm -rf nyarchtour.flatpak
-rm -rf wizard.flatpak
-rm -rf nyarchscript.flatpak
-rm -rf waifudownloader.flatpak
-rm -rf nyarchassistant.flatpak
 
-wget https://github.com/nyarchlinux/nyarchtour/releases/latest/download/nyarchtour.flatpak
-wget https://github.com/nyarchlinux/nyarchwizard/releases/latest/download/wizard.flatpak
-wget https://github.com/nyarchlinux/catgirldownloader/releases/latest/download/catgirldownloader.flatpak
-wget https://github.com/nyarchlinux/nyarchscript/releases/latest/download/nyarchscript.flatpak
-wget https://github.com/nyarchlinux/nyarchcustomize/releases/latest/download/nyarchcustomize.flatpak
-wget https://github.com/nyarchlinux/waifudownloader/releases/latest/download/waifudownloader.flatpak
-wget https://github.com/nyarchlinux/nyarchassistant/releases/latest/download/nyarchassistant.flatpak
+curl https://github.com/nyarchlinux/nyarchtour/releases/latest/download/nyarchtour.flatpak > nyarchtour.flatpak
+curl https://github.com/nyarchlinux/nyarchwizard/releases/latest/download/wizard.flatpak > wizard.flatpak
+curl https://github.com/nyarchlinux/catgirldownloader/releases/latest/download/catgirldownloader.flatpak > catgirldownloader.flatpak
+curl https://github.com/nyarchlinux/nyarchscript/releases/latest/download/nyarchscript.flatpak > nyarchscript.flatpak
+curl https://github.com/nyarchlinux/nyarchcustomize/releases/latest/download/nyarchcustomize.flatpak > nyarchcustomize.flatpak
+curl https://github.com/nyarchlinux/waifudownloader/releases/latest/download/waifudownloader.flatpak > waifudownloader.flatpak
+curl https://github.com/nyarchlinux/nyarchassistant/releases/latest/download/nyarchassistant.flatpak > nyarchassistant.flatpak
 
 flatpak install nyarchtour.flatpak
 flatpak install catgirldownloader.flatpak
