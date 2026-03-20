2cd58372d0558bad2c199a709a6164be5342eeb4
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2026-03-20 17:11:18 +0100

    Switch to packages

--- Gnome/customize_airootfs.sh
+++ Gnome/customize_airootfs.sh
@@ -12,32 +12,10 @@ pacman-key --init
 pip install materialyoucolor --break-system-packages
 # Install Faltpaks
 flatpak mask "org.freedesktop.Platform.GL.nvidia*"
-
 flatpak install -y flathub com.github.tchx84.Flatseal
 flatpak install -y org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark
-
-wget https://github.com/nyarchlinux/nyarchtour/releases/latest/download/nyarchtour.flatpak
-wget https://github.com/nyarchlinux/nyarchwizard/releases/latest/download/wizard.flatpak
-wget https://github.com/nyarchlinux/catgirldownloader/releases/latest/download/catgirldownloader.flatpak
-wget https://github.com/nyarchlinux/nyarchscript/releases/latest/download/nyarchscript.flatpak
-wget https://github.com/nyarchlinux/nyarchcustomize/releases/latest/download/nyarchcustomize.flatpak
-wget https://github.com/nyarchlinux/nyarchassistant/releases/latest/download/nyarchassistant.flatpak
 wget https://github.com/nyarchlinux/nyarchupdater/releases/latest/download/nyarchupdater.flatpak
-
-flatpak install -y nyarchtour.flatpak
-flatpak install -y catgirldownloader.flatpak
-flatpak install -y nyarchcustomize.flatpak
-flatpak install -y wizard.flatpak
-flatpak install -y nyarchscript.flatpak
-flatpak install -y nyarchassistant.flatpak
 flatpak install -y nyarchupdater.flatpak
-
-rm -rf nyarchtour.flatpak
-rm -rf catgirldownloader.flatpak
-rm -rf nyarchcustomize.flatpak
-rm -rf wizard.flatpak
-rm -rf nyarchscript.flatpak
-rm -rf nyarchassistant.flatpak
 rm -rf nyarchupdater.flatpak
 
 flatpak --remove mask "org.freedesktop.Platform.GL.nvidia*"
